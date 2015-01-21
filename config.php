4699aced9ced17cdbc39b626267d09ee11145095
Lloric <emorickfighter@gmail.com>
2015-01-21 10:52:12 +0800

    :)

--- config.php
+++ config.php
@@ -5,9 +5,9 @@
      * @copyright 2015
      */
 
-    define('DB_SERVER', 'localhost');       //hostname sql.byethost11.org
-    define('DB_USER', 'root');                      //database username mardagzc_admin
-    define('DB_PASSWD', '');                        //database password qezephekaFr&$=
+    define('DB_SERVER', 'localhost');       //by mardags
+    define('DB_USER', 'root');                      //
+    define('DB_PASSWD', 'qazwsx');                        //
     define('DB_NAME', 'MyShopDB');
      
     define("DIR_NAME", (dirname(__FILE__)));
